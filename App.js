fa49e20b9e4956526fb30293252c12dfd84758b8
CarlosJohncentVentura <johncent.developer@gmail.com>
2025-08-15 10:09:42 +0800

    tutorial-12-alerts

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import {
   ScrollView,
   FlatList,
   TouchableOpacity,
+  Alert,
 } from "react-native";
 import Header from "./components/header";
 import TodoItem from "./components/todoItem";
@@ -27,9 +28,18 @@ export default function App() {
   };
 
   const submitHandler = (text) => {
-    setTodos((prevTodos) => {
-      return [{ text: text, key: Math.random().toString() }, ...prevTodos];
-    });
+    if (text.length > 3) {
+      setTodos((prevTodos) => {
+        return [{ text: text, key: Math.random().toString() }, ...prevTodos];
+      });
+    } else {
+      Alert.alert("OOPS", "Todos must be over 3 chars longs", [
+        {
+          text: "Understood",
+          onPress: () => console.log("Alert closed"),
+        },
+      ]);
+    }
   };
 
   return (
