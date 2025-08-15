49d7402ec8e3e7831e6c6d801341e39f3bf975e7
CarlosJohncentVentura <johncent.developer@gmail.com>
2025-08-15 14:59:32 +0800

    tutorial-17-custom-fonts

--- App.js
+++ App.js
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import * as Font from "expo-font";
 import Home from "./screens/home";
 
+//In the tutorial, AppLoading & expo-app-loading are used but
+//AppLoading & expo-app-loading are deprecated, use SplashScreen instead
+import * as SplashScreen from "expo-splash-screen";
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
-  return <Home />;
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    async function fetchFonts() {
+      await Font.loadAsync({
+        "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
+        "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+      });
+      setFontsLoaded(true);
+      await SplashScreen.hideAsync();
+    }
+    fetchFonts();
+  }, []);
+
+  if (fontsLoaded) {
+    return <Home />;
+  }
 }
